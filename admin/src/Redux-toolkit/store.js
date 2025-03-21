23af1d8c870b581be4b46824d789a6cf31ad48d5
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-03-21 15:27:29 +0530

    update store

--- admin/src/Redux-toolkit/store.js
+++ admin/src/Redux-toolkit/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./Slice/productSlice";
 import authSlice from "./Slice/authSlice";
+import orderSlice from "./Slice/orderSlice";
 
 
 const store = configureStore({
     reducer:{
         Product:productSlice,
-        auth:authSlice
+        auth:authSlice,
+        order:orderSlice
     }
 })
 
