b88caf2076a0bf406f23caf63e7496a1b7930257
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-03-21 15:27:12 +0530

    changes sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,40 +1,64 @@
-import React from 'react'
-import {FaPlus, FaShoppingCart } from "react-icons/fa";
+import React from "react";
+import { FaPlus, FaShoppingCart } from "react-icons/fa";
 import { AiOutlineProduct } from "react-icons/ai";
-import { Link } from 'react-router-dom';
- function Sidebar({setIsSidebarOpen}) {
+import { Link, redirect, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+
+function Sidebar({ setIsSidebarOpen }) {
+  const dispatch = useDispatch();
+const navigate  = useNavigate()
   return (
     <>
-       <nav className="px-2 mt-5">
-            <ul className="space-y-3">
-              <li className="flex items-center justify-center md:justify-start gap-3">
-                <Link to={"/addproduct"} className="flex gap-4 items-center rounded hover:bg-gray-200 p-3">
-                <FaPlus className="text-xl text-amber-600" />
-                <div className="md:ml-2 md:inline hidden" onClick={() => setIsSidebarOpen(false)}>
-                  Add Product
-                  </div>
-                </Link>
-              </li>
-              <li className=" flex   items-center justify-center md:justify-start">
-                <Link to={"/getproducts"} className="flex gap-4 items-center rounded  hover:bg-gray-200 p-3" >
-                <AiOutlineProduct className="text-xl  text-amber-600" />
-                <div className="md:ml-2 md:inline hidden" onClick={() => setIsSidebarOpen(false)}>
-                  Products
-                  </div>
-                </Link>
-              </li>
-              <li className="flex items-center justify-center md:justify-start">
-                <Link to={"/order"} className="flex rounded gap-4 hover:bg-gray-200 p-3" >
-                <FaShoppingCart className="text-xl  text-amber-600" />
-                <div className="md:ml-2 md:inline hidden" onClick={() => setIsSidebarOpen(false)}>
-                  Order List
-                </div>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+      <nav className="mt-5 px-2">
+        <ul className="space-y-3">
+          <li className="flex justify-center gap-3 items-center md:justify-start">
+            <Link
+              to={"/addproduct"}
+              className="flex p-3 rounded gap-4 hover:bg-gray-200 items-center"
+            >
+              <FaPlus className="text-amber-600 text-xl" />
+              <div
+                className="hidden md:inline md:ml-2"
+                onClick={() => setIsSidebarOpen(false)}
+              >
+                Add Product
+              </div>
+            </Link>
+          </li>
+          <li className="flex justify-center items-center md:justify-start">
+            <Link
+              to={"/"}
+              className="flex p-3 rounded gap-4 hover:bg-gray-200 items-center"
+            >
+              <AiOutlineProduct className="text-amber-600 text-xl" />
+              <div
+                className="hidden md:inline md:ml-2"
+                onClick={() => setIsSidebarOpen(false)}
+              >
+                Products
+              </div>
+            </Link>
+          </li>
+          <li className="flex justify-center items-center md:justify-start">
+            <Link
+              to={"/order"}
+              className="flex p-3 rounded gap-4 hover:bg-gray-200"
+            >
+              <FaShoppingCart className="text-amber-600 text-xl" />
+              <div
+                className="hidden md:inline md:ml-2"
+                onClick={() => setIsSidebarOpen(false)}
+              >
+                Order Management
+              </div>
+            </Link>
+          </li>
+         
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Sidebar;
