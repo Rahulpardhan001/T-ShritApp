6f5cd1a2db684e76c7c3d2594ee457f53a6601fe
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-03-27 18:33:33 +0530

    cartcontroller handler debug

--- backend/controller/cartCtrl.js
+++ backend/controller/cartCtrl.js
@@ -5,8 +5,9 @@ const responseHandler = require("../utills/responseHandler");
 
 const addToCart = async (req, res) => {
   try {
-    const { userId, productId, quantity } = req.body;
-    // const user = req.user._id;
+    const { productId, quantity } = req.body;
+    // const userId = req.user._id;
+    const userId = '67d94d5332988ea09c6faa88'
     const productfind = await productModel.findById(productId);
     // console.log(productfind,"product found")
     let cart = await CartModel.findOne({ userId });
@@ -23,7 +24,7 @@ const addToCart = async (req, res) => {
       cart.items.push({ productId, quantity });
     }
     await cart.save();
-    responseHandler.success(res, "item added to cart");
+    responseHandler.success(res,cart, "item added to cart");
   } catch (error) {
     console.log(error);
     responseHandler.error(res, error);
@@ -56,21 +57,22 @@ const getAllCartItemCtrl = async (req, res) => {
 // *******************Delete cart items *****************//
 const deleteCartItemCtrl = async (req, res) => {
   try {
-    const { userId, productId } = req.body;
-    const cart = await CartModel.findOne({ userId });
+    const { id } = req.params;
+    const userId = '67d94d5332988ea09c6faa88'
+    let cart = await CartModel.findOne({ userId });
     // console.log(cart);
     if (!cart) {
       return responseHandler.error(res, "cart not found", 404);
     }
-    cart.items = cart.items.filter(
-      (item) => item.productId.toString() !== productId
-    );
-    // console.log(cart, "updated cart");
+    // cart.items = cart.items.filter((item) =>console.log(item.productId,"itemsli"));
+    cart.items = cart.items.filter((item) => item.productId.toString() !== id);
+    console.log(cart, "updated cart",id);
+    
     await cart.save();
     return responseHandler.success(
       res,
       cart,
-      "Item remove in cart Successfuly"
+      "Item remove in cart Successfuly",
     );
   } catch (error) {
     console.log(error);
@@ -81,26 +83,55 @@ const deleteCartItemCtrl = async (req, res) => {
 // *******************update cart items *****************//
 const updateCartItemCtrl = async (req, res) => {
   try {
-    const {userId,productId, newquantity} = req.body;
-    let cart = await CartModel.findOne({userId});
-    console.log(cart)
-    const findIndex = cart.items.findIndex((item)=>item.productId.toString() === productId)
-    if(findIndex === -1){
-        return responseHandler.error(res,'product not found in cart', 404)
+    const { productId, newquantity } = req.body;
+    const userId = req.user._id;
+    // Validate request body
+    if (!userId || !productId || !newquantity || newquantity <= 0) {
+      return responseHandler.error(res, "Invalid request data", 400);
     }
-    if(newquantity >0){
-        cart.items[findIndex].quantity = newquantity
-    }else {
-        cart.items.splice(findIndex,1)
-    }
-    console.log(newquantity,"new")
-    await cart.save();
-    responseHandler.success(res, cart, "update cart successfully")
 
+    const product = await productModel.findById(productId);
+    if (!product) {
+      return responseHandler.error(res, "product not found", 404);
+    }
+
+    if (product.stock === 0) {
+      return responseHandler.success(res, "product Out of stock", 400);
+    }
+
+    let cart = await CartModel.findOne({ userId });
+    const findIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+    if (findIndex === -1) {
+      return responseHandler.error(res, "product not found in cart", 404);
+    }
+
+    // Check if requested quantity exceeds available stock
+    if (newquantity > product.stock) {
+      return responseHandler.success(
+        res,
+        "Requested quantity exceeds available stock",
+        400
+      );
+    }
+    if (newquantity > 0) {
+      cart.items[findIndex].quantity = newquantity;
+    } else {
+      cart.items.splice(findIndex, 1);
+    }
+    console.log(newquantity, "new");
+    await cart.save();
+    responseHandler.success(res, cart, "update cart successfully");
   } catch (error) {
     console.log(error);
     return responseHandler.error(res, error);
   }
 };
 
-module.exports = { addToCart, getAllCartItemCtrl, deleteCartItemCtrl,updateCartItemCtrl };
+module.exports = {
+  addToCart,
+  getAllCartItemCtrl,
+  deleteCartItemCtrl,
+  updateCartItemCtrl,
+};
