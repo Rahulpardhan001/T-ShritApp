d893385f1af9b78100bb252cd3006f65d0cb3564
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-01 23:22:19 +0530

    public get product

--- backend/Routes/productRoute.js
+++ backend/Routes/productRoute.js
@@ -7,7 +7,7 @@ const ProductRoute = express.Router()
 ProductRoute.post("/addproduct",upload.fields([{name:"image",maxCount:4}]),authmiddleware,isAdmin,addProductCtrl)
 ProductRoute.delete('/deleteproduct/:id',authmiddleware,isAdmin,deleteproductctrl)
 ProductRoute.put('/updateproduct/:id',upload.fields([{name:"image",maxCount:4}]),authmiddleware,isAdmin,updateproudctctrl)
-ProductRoute.get("/getproduct",authmiddleware,getproductCtrl )
+ProductRoute.get("/getproduct",getproductCtrl )
 ProductRoute.get('/getsingleproduct/:id',authmiddleware,getSingleProductctrl)
 ProductRoute.get('/search',searchctrl)
 module.exports = ProductRoute;
