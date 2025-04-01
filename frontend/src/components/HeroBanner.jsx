471813cb787351c609d9cb909fa2f38a6a483559
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-01 18:54:16 +0530

    update hero section

--- frontend/src/components/HeroBanner.jsx
+++ frontend/src/components/HeroBanner.jsx
@@ -1,106 +1,67 @@
 import React, { useRef } from "react";
 import Slider from "react-slick";
-// Import Slick Carousel CSS at the top
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from "react-icons/io";
+
 function HeroBanner() {
   return (
-    <>
-      <div className='heroBanner flex flex-col md:flex-row justify-between  w-[100%] px-10 py-4'>
-        <div className='flex flex-col  leading-loose border-r text-xl w-[30%] '>
-            <div className='flex justify-between items-center'>
-            <Link>Woman's Fashion</Link>
-            <IoIosArrowForward/>
-            </div>
-
-            <span className='flex items-center justify-between'>
-            <Link>Men's Fashion</Link>
-            <IoIosArrowForward/>
-            </span>
-            <span>
-            <Link>Electronics</Link>
-            </span>
-
-            <span>
-            <Link>Home & LifeStyle</Link>
-            </span>
-            <span>
-            <Link>Medicine</Link>
-            </span>
-            <span>
-            <Link>Sports & Outdoor</Link>
-            </span>
-            <span>
-            <Link>Baby's & Toys</Link>
-            </span>
-            <span>
-            <Link>Groceries & Pets</Link>
-            </span>
-            <span>
-            <Link>Health & Beauty</Link>
-            </span>
-          
-        </div>
-        <div className='flex items-center p-8 col-span-1 lg:col-span-3  w-[70%] '>
-            {/* <img src="../src/assets/img/Frame 560.png" alt="" /> */}
-            <AutoPlayMethods/>
-        </div>
+    <div className='heroBanner flex flex-col md:flex-row justify-between w-full px-6 md:px-10 py-6'>
+      {/* Category Sidebar */}
+      <div className='flex flex-col space-y-3 text-lg md:text-xl w-full md:w-1/4 border-r pr-4'>
+        {[
+          "Woman's Fashion",
+          "Men's Fashion",
+          "Electronics",
+          "Home & Lifestyle",
+          "Medicine",
+          "Sports & Outdoor",
+          "Baby's & Toys",
+          "Groceries & Pets",
+          "Health & Beauty"
+        ].map((category, index) => (
+          <div key={index} className='flex justify-between items-center hover:text-blue-500 transition duration-300 cursor-pointer'>
+            <Link>{category}</Link>
+            {index < 2 && <IoIosArrowForward />}
+          </div>
+        ))}
       </div>
-    </>
-  )
+
+      {/* Image Slider */}
+      <div className='w-full md:w-3/4 flex items-center p-4'>
+        <AutoPlaySlider />
+      </div>
+    </div>
+  );
 }
 
-export default HeroBanner
+export default HeroBanner;
 
-
-
-function AutoPlayMethods() {
-  let sliderRef = useRef(null);
-  const play = () => {
-    sliderRef.slickPlay();
-  };
-  const pause = () => {
-    sliderRef.slickPause();
-  };
-
+function AutoPlaySlider() {
+  const sliderRef = useRef(null);
+  
   const settings = {
     dots: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 3000,
+    arrows: false
   };
+
   return (
-    <div className="slider-container w-[100%]">
-      <Slider className="" ref={slider => (sliderRef = slider)} {...settings}>
-        <div className="h-96 w-full">
-            <img className="h-full w-full" src="../src/assets/img/Frame 560.png" alt="" />
-        </div>
-        <div className="h-96 w-full">
-        <img className="h-full w-full" src="../src/assets/img/imageB1.png" alt="" />
-        </div>
-        <div className="h-96 w-full">
-        <img className="h-full w-full" src="../src/assets/img/imageB2.png" alt="" />
-        </div>
-        <div className="h-96 w-full">
-        <img className="h-full w-full" src="../src/assets/img/imageB3.png" alt="" />
-
-        </div>
-
+    <div className='w-full rounded-lg overflow-hidden shadow-lg'>
+      <Slider ref={(slider) => (sliderRef.current = slider)} {...settings}>
+        {["Frame 560.png", "imageB1.png", "imageB2.png", "imageB3.png"].map((img, index) => (
+          <div key={index} className='h-64 md:h-96 w-full'>
+            <img className='h-full w-full object-cover rounded-lg' src={`../src/assets/img/${img}`} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
       </Slider>
-      {/* <div style={{ textAlign: "center" }}>
-        <button className="button" onClick={play}>
-          Play
-        </button>
-        <button className="button" onClick={pause}>
-          Pause
-        </button>
-      </div> */}
     </div>
   );
 }
+
 
