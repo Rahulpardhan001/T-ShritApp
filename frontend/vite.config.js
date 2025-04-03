aa132662f899db20b5a3c0cf2a9fbaa926647158
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-03 15:17:54 +0530

    add proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':'https://t-shritapp.onrender.com'
+    }
+  },
   plugins: [react()],
 })
