bf200a5ab7dd79707205de3bea953f1880795c52
Rahulpardhan001 <rahulpardhan2001@gmail.com>
2025-04-03 13:38:36 +0530

    add all origin

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ const wishlistRoute = require('./Routes/wishlisthRoute')
 // const connectCloudinary = require('./utills/cloudnary');
 const corsOptions = {
   origin: [
-    "http://localhost:5173", 
-    "http://localhost:5174",
-    "https://merntshirtapp-rahul.netlify.app/"
+   "*"
   ],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
 };
